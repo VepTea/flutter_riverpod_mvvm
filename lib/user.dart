de1ff69abe449678176bba41eb20ba2c9802a46c
VepTea <97ghddlstjs@gmail.com>
2025-08-02 18:19:33 +0900

    서버에서 데이터를 가지고 오는 역할을 하는 모델레이어를 만들어야함. lib에 모델클래스 생성함.

---
+++ lib/user.dart
@@ -0,0 +1,19 @@
+/*{
+	"name": "jiwon",
+	"age": 20
+}*/
+
+class User {
+  final String name;
+  final int age;
+
+  User({required this.name, required this.age});
+  // 1. 네임드 생성자 (fromJson) 만들고
+  User.fromJson(Map<String, dynamic> map)
+    : name = map['name'],
+      age = map['age'];
+
+  // 2. toJson 만들기
+  Map<String, dynamic> toJson() => {'name': name, 'age': age};
+}
+//모델 클래스 정의가 완료되었으니까, 데이터를 가지고오는 역할을 하는 레포지토리를 만들어야함.
